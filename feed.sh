950990bbdf2bb2c2e5e455ec85ce9ffb40180ca6
root <root@raspberrypidf.(none)>
2014-10-04 18:34:47 +0000

    some testing files

---
+++ feed.sh
@@ -0,0 +1,40 @@
+
+
+#home directory
+homedir=/var/www
+
+#min time between treets being distributed
+feedlimit=.15
+
+if test `find "lastfeed.txt" -mmin +"$feedlimit"`
+then
+    echo feeding the dogs > "$homedir"/lastfeed.txt
+echo script to drop food goes here
+echo 0=55 > /dev/servoblaster
+sleep 0.25
+echo 0=150 > /dev/servoblaster
+sleep 0.25
+echo 0=55 > /dev/servoblaster
+sleep 0.25
+echo 0=150 > /dev/servoblaster
+
+
+sleep 0.25
+echo 0=150 > /dev/servoblaster
+sleep 0.25
+echo 0=55 > /dev/servoblaster
+sleep 0.25
+echo 0=150 > /dev/servoblaster
+sleep 0.25
+echo 0=190 > /dev/servoblaster
+
+else
+	echo too much food, can only feed every "$feedlimit" minutes.
+
+#ls -lah lastfeed.txt | awk '{print \$8}'
+
+echo `find "lastfeed.txt"`
+
+fi
+
+
